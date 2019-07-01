ad73b6ba66e816e0f1b7399c55f8f955cb130c6e
Mohamed Amine MELLAL <mellalm@wataniya-algerie.com>
2019-07-01 14:59:09 +0100

    commit 3

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,53 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundry";
 
-const Details = props => {
+class Details extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    //throw new Error("lol");
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false
+        });
+      })
+      .catch(err => this.setState({ error: err }));
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, description, name, media } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+        <Carousel media={media} />
+      </div>
+    );
+  }
+}
+
+export default function DetailsErrorBoundary(props) {
   return (
-    <pre>
-      <code> {JSON.stringify(props, null, 4)}</code>
-    </pre>
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
   );
-};
-
-export default Details;
+}
